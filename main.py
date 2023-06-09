3fe0ab3077909ca29ab4a3b41a93b4915f6c1ea2
prostosahar <118665011+prostosahar@users.noreply.github.com>
2023-06-09 12:16:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -97,5 +97,20 @@ def one():
 def base():
     return render_template("base.html")
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    
+@app.get('/shutdown')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'    
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=5000)
+    
+   
+
+    
