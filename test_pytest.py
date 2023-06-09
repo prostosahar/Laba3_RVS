df0b3eb5b6a824ee3231c3a2cffdcd89ac985a06
prostosahar <118665011+prostosahar@users.noreply.github.com>
2023-06-09 13:19:41 +0700

    Update test_pytest.py

--- test_pytest.py
+++ test_pytest.py
@@ -1,9 +1,9 @@
 import unittest
-from main import *
+from main import suum
 
 class TestMain(unittest.TestCase):
     def test_my_func(self):
-        self.assertEqual(main, 0)
+        self.assertEqual(suum, 0)
 
         
 if __name__ == '__main__':
